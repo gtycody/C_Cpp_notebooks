0b353e9bd494c58e8225f4f8ae3d143c5ec3de4a
gtycody <gtycody@gmail.com>
2019-09-21 13:43:30 -0500

    VM_demo

---
+++ VM_demo_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("location of code: %p\n",main);
+    printf("location of heap: %p\n",malloc(100e6));
+    int x = 3;
+    printf("location of stack: %p\n", &x);
+    return 0;
+}
+
+
