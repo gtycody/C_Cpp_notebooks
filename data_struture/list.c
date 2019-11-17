327b631904adb790255a10d52f79894e689c9544
Tingyue Guo <gtycody@gmail.com>
2019-11-16 21:01:13 -0600

    list obj part1

--- data_struture/list.c
+++ data_struture/list.c
@@ -1,21 +1,37 @@
 // In this file I would use some most 
 // basic rules to achieve the "list" 
-// concept in python
+// concept in python Main idea to achieve a array of pointers that could contain
+// all types of variables.
+
+// In this array of pointers: 
+//      each pointer should be initialized as a void pointer which doesn't have type pre-defined
+//      each element a pointer pointing to should be warp with a char[] which contain its type info
+//      each element of pointer will have a pointer pointing to the object it represent.
+
+//EXAMPLE:
+// int main()
+// {
+//     int num1 = 1;
+//     char char1 = 'a';
+//     void* vp1;
+//     void* vp2;
+//     vp1 = &num1;
+//     vp2 = &char1;
+//     printf("%d,%c\n",*(int *)vp1,*(char *)vp2);
+// }
 
 #include <stdio.h>
+typedef struct list{
+    void* [] list_obj;
+}list;
 
-int main()
-{
-    int num1 = 1;
-    char char1 = 'a';
+typedef struct element{
+    char[] type_nanme;
+    void* object_ptr;
+}list_elem;
 
-    void* vp1;
-    void* vp2;
 
-    vp1 = &num1;
-    vp2 = &char1;
 
-    printf("%d,%c\n",*(int *)vp1,*(char *)vp2);
-}
+
 
 
