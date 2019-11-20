7c9cf1ff526982e6fb631108141c9d44b2f9bfd8
Tingyue Guo <gtycody@gmail.com>
2019-11-20 02:51:40 -0600

    beifeng

--- system/malloc_1.c
+++ system/malloc_1.c
@@ -3,13 +3,14 @@
 
 int main(){
     int a   = 10;
-    int* ptr = &a;
+    int* ptr = &a; //which is store in the stack 
     int* ptr1;
-    ptr1 = (int *)malloc(sizeof(a));
+    ptr1 = (int *)malloc(sizeof(a));//which is store at heap
     printf("%p\n",ptr);
     printf("%p\n",ptr1);
+    
     long distance;
     distance = ptr-ptr1;
-    printf("%lu\n",distance/1024/1024/1024);
+    printf("%lu\n",distance/1024/1024/1024); //distance between stack and heap
     return 0;
 }
